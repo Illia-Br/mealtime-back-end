76f62061c3a8cdd9cccb8cf3476be06bb7feb8ad
LukeSchaffel <lukeschaffel@gmail.com>
2022-03-24 16:31:14 -0400

    make meal schema

---
+++ models/meal.js
@@ -0,0 +1,33 @@
+import { mongoose } from "mongoose";
+
+const Schema = mongoose.schema
+
+const reviewSchema = new Schema({
+  content: String
+})
+
+
+const mealSchema = new Schema({
+  name: {type: String, required: true},
+  picture: {type: String},
+  ingredients: {type: String, required: true},
+  instructions: {type: String, required: true},
+  prepTime: {type: String},
+  creator: {type: mongoose.Schema.Types.ObjectId, ref: "Profile"},
+  calories: Number,
+  reviews: [reviewSchema],
+  restaurants: {
+    type: Schema.Types.ObjectId, ref: "Restaurant"
+  }
+
+
+}, {
+  timestamps: true
+})
+
+const Meal = mongoose.model('Meal', mealSchema)
+
+
+export {
+  Meal
+}
