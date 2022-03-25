52b5c9c9da881abd46b2d1508a254095c18cf95e
LukeSchaffel <lukeschaffel@gmail.com>
2022-03-25 11:30:06 -0400

    clean up code

--- routes/meals.js
+++ routes/meals.js
@@ -12,7 +12,7 @@ router.get('/', mealsCtrl.index)
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
-router.post('/', checkAuth, mealsCtrl.create)
+router.post('/', mealsCtrl.create)
 router.put('/:id', checkAuth, mealsCtrl.update)
 router.delete('/:id', checkAuth, mealsCtrl.delete)
 
